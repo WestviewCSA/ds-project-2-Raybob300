cce272461e976df090b35d12a5ae93e6d1221358
Raybob300 <rayhansh77@gmail.com>
2025-03-28 10:35:17 -0700

    Actually Done (Added the door/portal)

--- P2_first_last/src/QueueSolver.java
+++ P2_first_last/src/QueueSolver.java
@@ -1,156 +1,95 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class QueueSolver  {
-	  private char[][] maze;
-	    private int rows, cols;
-	    private int startX, startY;
-	    private static final char wall = '@';
-	    private static final char open = '.';
-	    private static final char path = '+';
-	    private static final char start = 'W';
-	    private static final char end = '$';
-	
-	public QueueSolver(char[][] beans) {
-	    this.maze = beans;
-        this.rows = maze.length;
-        this.cols = maze[0].length;
-        findStart();	}
-	
-	
-	public void findStart() {
+public class QueueSolver {
+    private char[][] maze;
+    private int rows, cols;
+    private int startX, startY;
+    private int portal1X = -1, portal1Y = -1, portal2X = -1, portal2Y = -1;
+    private int wolverine1X = -1, wolverine1Y = -1, wolverine2X = -1, wolverine2Y = -1;
+
+    public QueueSolver(char[][] inputMaze) {
+        this.rows = inputMaze.length;
+        this.cols = inputMaze[0].length;
+        this.maze = new char[rows][cols];
+
+        // Copy the maze into the internal representation and track positions of important characters
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (maze[i][j] == start) {
-                    startX = i;
-                    startY = j;
-                    return;
+                this.maze[i][j] = inputMaze[i][j];
+                if (maze[i][j] == 'W') {
+                    if (wolverine1X == -1) {
+                        wolverine1X = i;
+                        wolverine1Y = j;
+                    } else {
+                        wolverine2X = i;
+                        wolverine2Y = j;
+                    }
+                } else if (maze[i][j] == '|') {
+                    if (portal1X == -1) {
+                        portal1X = i;
+                        portal1Y = j;
+                    } else {
+                        portal2X = i;
+                        portal2Y = j;
+                    }
                 }
             }
         }
+        startX = wolverine1X;
+        startY = wolverine1Y;
     }
+  //finds path while looking for all options as . |
 
-    public void findPath() {
-        boolean[][] visited = new boolean[rows][cols];
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> queue = new LinkedList<int[]>();
-        int[][][] parent = new int[rows][cols][2]; 
-
+    public boolean findPath() {
+        Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{startX, startY});
-        visited[startX][startY] = true;   // if the square is visited it should be marked true
 
-        while (!queue.isEmpty()) {// checks if empty
+        while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int x = current[0], y = current[1];
 
-            if (maze[x][y] == end) {
-                markPath(parent, x, y);
-                return;
+            // If we reached the goal, stop
+            if (maze[x][y] == '$') return true;
+
+            // Mark the current path with '+' except if it's a portal
+            if (maze[x][y] != 'W' && maze[x][y] != '|') maze[x][y] = '+';
+
+            // Check movement directions (up, down, left, right)
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+            for (int[] dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                if (isValidMove(newX, newY)) {
+                    queue.add(new int[]{newX, newY});
+                }
             }
 
-            for (int[] dir : directions) {
-                int newX = x + dir[0], newY = y + dir[1];
-                if (isValidMove(newX, newY, visited)) {
-                    queue.add(new int[]{newX, newY});
-                    visited[newX][newY] = true;
-                    parent[newX][newY] = new int[]{x, y}; // Store parent
+            // Handle portals: only teleport if we're standing on a portal
+            if (maze[x][y] == '|') {
+                maze[x][y] = '|'; // Mark the portal as part of the path
+                if (x == portal1X && y == portal1Y) {
+                    queue.add(new int[]{wolverine2X, wolverine2Y});
+                } else if (x == portal2X && y == portal2Y) {
+                    queue.add(new int[]{wolverine1X, wolverine1Y});
                 }
             }
         }
+        return false;
     }
 
-    private void markPath(int[][][] parent, int endX, int endY) {
-        int x = endX, y = endY;
-        while (maze[x][y] != start) {
-            int P1 = parent[x][y][0];
-            int P2 = parent[x][y][1];
-
-            if (maze[x][y] != end) {
-                maze[x][y] = path;
-            }
-
-            x = P1;
-            y = P2;
-        }
-    }
-
-    private boolean isValidMove(int x, int y, boolean[][] visited) {
-        return x >= 0 && x < rows && y >= 0 && y < cols && maze[x][y] != wall && !visited[x][y]; // checks all 4 directions the wolvirene can go
+    private boolean isValidMove(int x, int y) {
+        return x >= 0 && x < rows && y >= 0 && y < cols && 
+               (maze[x][y] == '.' || maze[x][y] == '$' || maze[x][y] == '|');
     }
 
     public void printMaze() {
         for (char[] row : maze) {
-            System.out.println(new String(row));
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
         }
     }
-    
- 
-// getters and setters auto generated
-	public char[][] getMaze() {
-		return maze;
-	}
-
-	public void setMaze(char[][] maze) {
-		this.maze = maze;
-	}
-
-	public int getRows() {
-		return rows;
-	}
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-	public int getCols() {
-		return cols;
-	}
-
-	public void setCols(int cols) {
-		this.cols = cols;
-	}
-
-	public static char getWall() {
-		return wall;
-	}
-
-	public static char getOpen() {
-		return open;
-	}
-
-	public static char getPath() {
-		return path;
-	}
-
-	public static char getStart() {
-		return start;
-	}
-
-	public static char getEnd() {
-		return end;
-	}
-
-	public int getStartX() {
-		return startX;
-	}
-
-	public void setStartX(int startX) {
-		this.startX = startX;
-	}
-
-	public int getStartY() {
-		return startY;
-	}
-
-	public void setStartY(int startY) {
-		this.startY = startY;
-	}
-	
-	
-	
-
-
 }
-   
-
-
--- P2_first_last/src/StackSolver.java
+++ P2_first_last/src/StackSolver.java
@@ -1,94 +1,92 @@
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackSolver {
-	//var decleration
     private char[][] maze;
     private int rows, cols;
     private int startX, startY;
-    private static final char wall = '@';
-    private static final char open = '.';
-    private static final char path = '+';
-    private static final char start = 'W';
-    private static final char end = '$';
+    private int portal1X = -1, portal1Y = -1, portal2X = -1, portal2Y = -1; //var for where the portal/door is
+    private int wolverine1X = -1, wolverine1Y = -1, wolverine2X = -1, wolverine2Y = -1;
 
-    public StackSolver(char[][] maze) {
-        this.maze = maze;
-        this.rows = maze.length;
-        this.cols = maze[0].length;
-        findStart();
-    }
+    public StackSolver(char[][] inputMaze) {
+        this.rows = inputMaze.length;
+        this.cols = inputMaze[0].length;
+        this.maze = new char[rows][cols];
 
-    private void findStart() {//looks for the W
+        // Copy the maze into the internal representation and track positions of important characters
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (maze[i][j] == start) {
-                    startX = i;
-                    startY = j;
-                    return;
+                this.maze[i][j] = inputMaze[i][j];
+                if (maze[i][j] == 'W') {
+                    if (wolverine1X == -1) {
+                        wolverine1X = i;
+                        wolverine1Y = j;
+                    } else {
+                        wolverine2X = i;
+                        wolverine2Y = j;
+                    }
+                } else if (maze[i][j] == '|') {
+                    if (portal1X == -1) {
+                        portal1X = i;
+                        portal1Y = j;
+                    } else {
+                        portal2X = i;
+                        portal2Y = j;
+                    }
                 }
             }
         }
+        startX = wolverine1X;
+        startY = wolverine1Y;
     }
-
-    public void findPath() {// finds the parth and then mark using methods
-        boolean[][] visited = new boolean[rows][cols];
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Stack<int[]> stack = new Stack<int[]>();
-        int[][][] parent = new int[rows][cols][2];
-
+//finds path while looking for all options as . |
+    public boolean findPath() {
+        Stack<int[]> stack = new Stack<>();
         stack.push(new int[]{startX, startY});
-        visited[startX][startY] = true;
 
         while (!stack.isEmpty()) {
             int[] current = stack.pop();
             int x = current[0], y = current[1];
 
-            if (maze[x][y] == end) {
-                markPath(parent, x, y);
-                return;
+            // If we reached the goal, stop
+            if (maze[x][y] == '$') return true;
+
+            // Mark the current path with '+' except if it's a portal
+            if (maze[x][y] != 'W' && maze[x][y] != '|') maze[x][y] = '+';
+
+            // Check movement directions (up, down, left, right)
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+            for (int[] dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                if (isValidMove(newX, newY)) {
+                    stack.push(new int[]{newX, newY});
+                }
             }
 
-            for (int[] dir : directions) {
-                int newX = x + dir[0], newY = y + dir[1];
-                if (isValidMove(newX, newY, visited)) {
-                    stack.push(new int[]{newX, newY});
-                    visited[newX][newY] = true;
-                    parent[newX][newY] = new int[]{x, y}; // Store parent
+            if (maze[x][y] == '|') {
+                maze[x][y] = '|'; // Mark the portal as part of the path
+                if (x == portal1X && y == portal1Y) {
+                    stack.push(new int[]{wolverine2X, wolverine2Y});
+                } else if (x == portal2X && y == portal2Y) {
+                    stack.push(new int[]{wolverine1X, wolverine1Y});
                 }
             }
         }
+        return false;
     }
 
-    private void markPath(int[][][] parent, int endX, int endY) {//mark path using this method
-        int x = endX, y = endY;
-        while (maze[x][y] != start) {
-            int P1 = parent[x][y][0];
-            int P2 = parent[x][y][1];
-
-            if (maze[x][y] != end) {
-                maze[x][y] = path;
-            }
-
-            x = P1;
-            y = P2;
-        }
-    }
-
-    private boolean isValidMove(int x, int y, boolean[][] visited) {
-        return x >= 0 && x < rows && y >= 0 && y < cols && maze[x][y] != wall && !visited[x][y];
+    private boolean isValidMove(int x, int y) {
+        return x >= 0 && x < rows && y >= 0 && y < cols && 
+               (maze[x][y] == '.' || maze[x][y] == '$' || maze[x][y] == '|');
     }
 
     public void printMaze() {
         for (char[] row : maze) {
-            System.out.println(new String(row));
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
         }
     }
-
-    
-    
-    
-    
 }
-
-
